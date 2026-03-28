4583d4df616309478bbab8ac13f242bb9f036a71
Rishika <doladevisriprasad050505@gmail.com>
2026-03-28 09:49:47 +0530

    Hybrid Revert: 6f9633e logic with SDK 54 infrastructure

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,8 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = false;
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = config;
